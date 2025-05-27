a0fa90dc32e46068b20e517eb6213afc70e2bf0f
AttiliManisha28 <attilimanisha28@gmail.com>
2025-05-27 15:32:25 +0530

    Create EX-21_Custom_Exception.java

---
+++ MODULE-3/EX-21_Custom_Exception.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+// Define custom exception
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class CustomExceptionDemo {
+    public static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be at least 18.");
+        } else {
+            System.out.println("Age is valid.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        try {
+            checkAge(age);
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+
+        scanner.close();
+    }
+}
